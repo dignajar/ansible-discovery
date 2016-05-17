967537abf7ee7715be765c2f44413cbc424583ad
Diego Najar <dignajar@gmail.com>
2016-05-17 18:04:48 -0300

    Updates

---
+++ www/hosts/test.your-domain.com
@@ -0,0 +1,5 @@
+{
+	"hostname":"udp01.despexds.net",
+	"os":"centos",
+	"time":"2016-05-17 10:59:41"
+}
--- www/index.php
+++ www/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php header('Content-Type: application/json');
 
 // Domain
 define('DOMAIN', 'your-domain.com');
